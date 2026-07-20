d29562828ac09a3d830f3b9b4f5044b8aa99ab5b
fabiomont2-jpg <fabio.mont2@gmail.com>
2026-07-20 19:00:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -183,9 +183,9 @@ def admin_acao(id, acao):
     db.session.commit()
     return jsonify({'success': True})
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
